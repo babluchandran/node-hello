463d851cf346db08ce1896ef2f7b30b6d85b811e
babluchandran <mailtobabluchandran@gmail.com>
2022-08-29 14:35:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
+  const msg = '<p> Express </p>'
+  //const msg1 = 'Welcome to Express\n'
+  res.end(msg.bold()   + '\r\n' + '<p> Welcome to Express </P>');
+ 
 });
+
 
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
